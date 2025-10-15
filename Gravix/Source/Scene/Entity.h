a1b27bbc8153db5feac48ed028ac42604748cf1a
DarkerMinecraft <nayongbin8@gmail.com>
2025-10-14 21:57:10 -0400

    Refactor Entity component methods and add const variants
    Updated Entity class to use entt::registry::all_of for HasComponent checks, added const versions of GetComponent and HasComponent, fixed RemoveComponent template syntax, and initialized m_EntityHandle to entt::null. Also added explicit bool operator for validity checks.

--- Gravix/Source/Scene/Entity.h
+++ Gravix/Source/Scene/Entity.h
@@ -5,18 +5,18 @@
 
 #include <entt/entt.hpp>
 
-namespace Gravix 
+namespace Gravix
 {
 
-	class Entity 
+	class Entity
 	{
 	public:
-		Entity(entt::entity handle, Scene* scene) 
-			: m_EntityHandle(handle), m_Scene(scene) { }
+		Entity(entt::entity handle, Scene* scene)
+			: m_EntityHandle(handle), m_Scene(scene) {}
 		Entity(const Entity&) = default;
 
 		template<typename T, typename... Args>
-		T& AddComponent(Args&&... args) 
+		T& AddComponent(Args&&... args)
 		{
 			GX_CORE_ASSERT(!HasComponent<T>(), "Entity already has componenet!");
 
@@ -24,7 +24,7 @@ namespace Gravix
 		}
 
 		template<typename T>
-		T& GetComponent() 
+		T& GetComponent()
 		{
 			GX_CORE_ASSERT(HasComponent<T>(), "Entity does not has componenet!");
 
@@ -32,7 +32,15 @@ namespace Gravix
 		}
 
 		template<typename T>
-		void RemoveComponent<T>() 
+		const T& GetComponent() const
+		{
+			GX_CORE_ASSERT(HasComponent<T>(), "Entity does not has componenet!");
+
+			return m_Scene->m_Registry.get<T>(m_EntityHandle);
+		}
+
+		template<typename T>
+		void RemoveComponent()
 		{
 			GX_CORE_ASSERT(HasComponent<T>(), "Entity does not has componenet!");
 
@@ -40,9 +48,15 @@ namespace Gravix
 		}
 
 		template<typename T>
-		bool HasComponent() 
+		bool HasComponent()
 		{
-			return m_Scene->m_Registry.has<T>(m_EntityHandle);
+			return m_Scene->m_Registry.all_of<T>(m_EntityHandle);
+		}
+
+		template<typename T>
+		const bool HasComponent() const
+		{
+			return m_Scene->m_Registry.all_of<T>(m_EntityHandle);
 		}
 
 		glm::mat4& GetTransform() { return GetComponent<TransformComponent>(); }
@@ -50,8 +64,10 @@ namespace Gravix
 
 		const glm::mat4& GetTransform() const { return GetComponent<TransformComponent>(); }
 		const UUID& GetID() const { return GetComponent<TagComponent>(); }
+
+		operator bool() const { return m_EntityHandle != entt::null; }
 	private:
-		entt::entity m_EntityHandle;
+		entt::entity m_EntityHandle{ entt::null };
 		Scene* m_Scene;
 	};
 
