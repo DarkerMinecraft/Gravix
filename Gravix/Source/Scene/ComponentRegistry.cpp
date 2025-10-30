c782b47c77e43edd1fbe9c867bee56884d1756a8
DarkerMinecraft <nayongbin8@gmail.com>
2025-10-30 16:31:14 -0400

    Refactor DrawVec3Control to file scope function
    Moved the DrawVec3Control lambda from inside the TransformComponent registration to a static file-scope function. This improves code reuse and readability by avoiding duplication and making the function accessible throughout the file.

--- Gravix/Source/Scene/ComponentRegistry.cpp
+++ Gravix/Source/Scene/ComponentRegistry.cpp
@@ -6,8 +6,70 @@
 #include <imgui.h>
 #include <imgui_internal.h>
 
-namespace Gravix 
+namespace Gravix
 {
+
+	static void DrawVec3Control(const char* label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f)
+	{
+		ImGuiIO& io = ImGui::GetIO();
+
+		ImGui::PushID(label);
+
+		ImGui::Columns(2);
+		ImGui::SetColumnWidth(0, columnWidth);
+		ImGui::Text(label);
+		ImGui::NextColumn();
+
+		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		float lineHeight = ImGui::GetFontSize() + ImGui::GetStyle().FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+		// X Axis (Red)
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+		if (ImGui::Button("X", buttonSize))
+			values.x = resetValue;
+		ImGui::PopStyleColor(3);
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		// Y Axis (Green)
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+		if (ImGui::Button("Y", buttonSize))
+			values.y = resetValue;
+		ImGui::PopStyleColor(3);
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
+		ImGui::PopItemWidth();
+		ImGui::SameLine();
+
+		// Z Axis (Blue)
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+		if (ImGui::Button("Z", buttonSize))
+			values.z = resetValue;
+		ImGui::PopStyleColor(3);
+
+		ImGui::SameLine();
+		ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
+		ImGui::PopItemWidth();
+
+		ImGui::PopStyleVar();
+
+		ImGui::Columns(1);
+
+		ImGui::PopID();
+	};
 
 	void ComponentRegistry::RegisterAllComponents()
 	{
@@ -33,68 +95,6 @@ namespace Gravix
 			nullptr,
 			[](TransformComponent& c)
 			{
-				auto DrawVec3Control = [](const char* label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f)
-					{
-						ImGuiIO& io = ImGui::GetIO();
-
-						ImGui::PushID(label);
-
-						ImGui::Columns(2);
-						ImGui::SetColumnWidth(0, columnWidth);
-						ImGui::Text(label);
-						ImGui::NextColumn();
-
-						ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
-						ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
-
-						float lineHeight = ImGui::GetFontSize() + ImGui::GetStyle().FramePadding.y * 2.0f;
-						ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
-
-						// X Axis (Red)
-						ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
-						ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
-						ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
-						if (ImGui::Button("X", buttonSize))
-							values.x = resetValue;
-						ImGui::PopStyleColor(3);
-
-						ImGui::SameLine();
-						ImGui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f");
-						ImGui::PopItemWidth();
-						ImGui::SameLine();
-
-						// Y Axis (Green)
-						ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
-						ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
-						ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
-						if (ImGui::Button("Y", buttonSize))
-							values.y = resetValue;
-						ImGui::PopStyleColor(3);
-
-						ImGui::SameLine();
-						ImGui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f");
-						ImGui::PopItemWidth();
-						ImGui::SameLine();
-
-						// Z Axis (Blue)
-						ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
-						ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
-						ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
-						if (ImGui::Button("Z", buttonSize))
-							values.z = resetValue;
-						ImGui::PopStyleColor(3);
-
-						ImGui::SameLine();
-						ImGui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f");
-						ImGui::PopItemWidth();
-
-						ImGui::PopStyleVar();
-
-						ImGui::Columns(1);
-
-						ImGui::PopID();
-					};
-
 				// Draw the Transform component UI
 				DrawVec3Control("Position", c.Position);
 				DrawVec3Control("Rotation", c.Rotation);
@@ -111,11 +111,11 @@ namespace Gravix
 
 		RegisterComponent<CameraComponent>(
 			"CameraCompoent",
-			[](CameraComponent& c, Scene* scene) 
+			[](CameraComponent& c, Scene* scene)
 			{
 
 			},
-			[](CameraComponent& c) 
+			[](CameraComponent& c)
 			{
 
 			}
