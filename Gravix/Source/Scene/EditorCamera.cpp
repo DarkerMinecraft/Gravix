f93d7d2f8390268f72c38f8f2707d0962221461c
DarkerMinecraft <nayongbin8@gmail.com>
2025-10-31 08:04:55 -0400

    Adjust projection matrices for Vulkan coordinate system
    Defined GLM_FORCE_DEPTH_ZERO_TO_ONE to match Vulkan's depth range and inverted the Y axis in projection matrices for both EditorCamera and SceneCamera. This ensures correct rendering orientation and depth mapping when using Vulkan.

--- Gravix/Source/Scene/EditorCamera.cpp
+++ Gravix/Source/Scene/EditorCamera.cpp
@@ -19,6 +19,8 @@ namespace Gravix
 	{
 		m_AspectRatio = m_ViewportWidth / m_ViewportHeight;
 		m_ProjectionMatrix = glm::perspective(glm::radians(m_FOV), m_AspectRatio, m_NearClip, m_FarClip);
+
+		m_ProjectionMatrix[1][1] *= -1.0f;
 	}
 
 	void EditorCamera::UpdateView()
